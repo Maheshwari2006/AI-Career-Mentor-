c0637bbc07cdf950021cc403d3f31eb2fc9a9e0b
Maheshwari2006 <rewatkarmaheshwari@gmail.com>
2026-06-16 11:41:54 +0530

    Fix resume_parser syntax error

--- nlp/resume_parser.py
+++ nlp/resume_parser.py
@@ -6,8 +6,6 @@ import pytesseract
 from pdf2image import convert_from_path
 from nlp.skills_db import SKILLS
 
-nlp/resume_parser.py
-
 # Tesseract Path
 pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
